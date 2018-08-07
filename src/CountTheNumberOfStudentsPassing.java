dfa93faee6e84395e240a9efd4738f0d0ea8f6fb
Tien Dat <datdta95@gmail.com>
2018-08-07 15:01:51 +0700

    Nhập điểm cho sinh viên và kiểm tra điểm số

--- src/CountTheNumberOfStudentsPassing.java
+++ src/CountTheNumberOfStudentsPassing.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class CountTheNumberOfStudentsPassing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //Kiểm tra số lượng sinh viên được nhập tối đa là 30
         int numberOfStudents;
         do {
             System.out.println("Nhập vào số sinh viên:");
@@ -11,6 +13,18 @@ public class CountTheNumberOfStudentsPassing {
                 System.out.println("Số sinh viên tối đa là 30");
             }
         } while (numberOfStudents > 30);
+
+        //Nhập điểm cho sinh viên
+        int[] pointOfStudent = new int[numberOfStudents];
+        for (int i = 0; i < pointOfStudent.length; i++) {
+            do {
+                System.out.println("Nhập vào điểm sinh viên thứ " + (i + 1));
+                pointOfStudent[i] = scanner.nextInt();
+                if (pointOfStudent[i] < 0 || pointOfStudent[i] > 10) {
+                    System.out.println("Nhập lại điểm của sinh viên thứ " + (i + 1));
+                }
+            } while (pointOfStudent[i] < 0 || pointOfStudent[i] > 10);
+        }
 
 
     }
